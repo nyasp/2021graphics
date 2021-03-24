d67d5de5a37a9d0a2a38ba5635335eaf5d321e69
nyasp <80370077+nyasp@users.noreply.github.com>
2021-03-24 12:05:13 +0800

    Add files via upload

---
+++ week05/main.cpp
@@ -0,0 +1,47 @@
+
+#include <GL/glut.h>
+int N=0,vx[3000],vy[3000];
+float a=0;
+void display()
+{
+
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glPushMatrix();
+
+        glRotatef(a,0,0,1);
+
+        glColor3d(1,0,0);
+        glBegin(GL_POLYGON);
+            for(int i=0;i<N;i++) glVertex2f(((vx[i]-150)/150.0),-((vy[i]-150)/150.0));
+        glEnd();
+    glPopMatrix();
+    glutSwapBuffers();
+}
+
+void keyboard(unsigned char key,int x,int y)
+{
+    a+=2;
+
+    display();
+}
+void motion( int x , int y )
+{
+    vx[N]=x;vy[N]=y;
+    N++;
+    display();
+}
+int main(int argc, char **argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode( GLUT_DOUBLE | GLUT_DEPTH);
+
+    glutCreateWindow("week05");
+
+
+    glutMotionFunc(motion);
+    glutKeyboardFunc(keyboard);
+    glutDisplayFunc(display);
+    glutMainLoop();
+}
